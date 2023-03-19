a93078805a077e2ac0f72cda62f3c33058a638a4
WildcardTech <38894634+WildcardTech@users.noreply.github.com>
2023-03-19 11:25:14 -0500

    Create csv_upate_emtpy.py

---
+++ csv_upate_emtpy.py
@@ -0,0 +1,20 @@
+import csv
+
+# Open the input CSV file
+with open('input.csv', 'r') as csvfile:
+    # Open the output CSV file
+    with open('output.csv', 'w', newline='') as outfile:
+        reader = csv.reader(csvfile)
+        writer = csv.writer(outfile)
+
+        # Loop through each row in the CSV file
+        for row in reader:
+            # Loop through each cell in the row
+            for i, cell in enumerate(row):
+                # Check if the cell is blank
+                if cell.strip() == '':
+                    # If the cell is blank, add a value
+                    row[i] = 'N/A'
+
+            # Write the updated row to the output CSV file
+            writer.writerow(row)
