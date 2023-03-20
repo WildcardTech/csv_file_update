fbfe6daaad06570856e211b5099275742e80cd7c
WildcardTech <38894634+WildcardTech@users.noreply.github.com>
2023-03-19 22:04:13 -0500

    Create csv_upate_emtpy2.py

---
+++ csv_upate_emtpy2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Read in the CSV file
+df = pd.read_csv('your_csv_file.csv')
+
+# Look for the ID column and check if it is blank or only 2 characters
+id_col = df['ID']
+for i, value in id_col.iteritems():
+    if pd.isnull(value) or len(str(value)) == 2:
+        # If the value is blank or only 2 characters, add something to the cell
+        df.at[i, 'ID'] = 'New ID'
+
+# Write the updated dataframe back to the CSV file
+df.to_csv('your_csv_file.csv', index=False)
+This code reads in a CSV file using
